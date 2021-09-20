24af92a23fa661b8bcf9f6e7cfbf39e56d29225d
DarkDevil1265 <90670797+DarkDevil1265@users.noreply.github.com>
2021-09-20 15:36:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🔰Join Channel🔰", url="https://t.me/malluinstafollowers"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("🔰Join Group🔰", url="https://t.me/malluinstaufollowers"),
+                    InlineKeyboardButton("details about Group", url="https://t.me/Malluinstabot")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/DarkDevil1265/Unlimited-filter-bot-RJ")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
