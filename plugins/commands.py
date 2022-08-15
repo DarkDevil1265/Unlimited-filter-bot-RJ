423b86bd51626c66c22b52c5db5ea5e0d0fc6760
jsaigopalreddy18 <93173040+jsaigopalreddy18@users.noreply.github.com>
2022-08-15 17:05:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -221,7 +221,7 @@ async def start(client, message):
                 ]
             ]
         ),
-        reply_to_message_id=message.message_id
+        reply_to_message_id=message.message.id
     )
     if Config.SAVE_USER == "yes":
         try:
@@ -273,5 +273,5 @@ async def about(client, message):
                 ]                
             ]
         ),
-        reply_to_message_id=message.message_id
+        reply_to_message_id=message.message.id
     )
